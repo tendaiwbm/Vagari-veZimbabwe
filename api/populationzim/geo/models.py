3a20a6be1621d36d8e9e6f90dc3499ea93940d36
tendaiwbm <tmbwanda52@gmail.com>
2026-08-03 09:36:11 +0200

    api: geo - models to create & manage users and test for creating users

--- api/populationzim/geo/models.py
+++ api/populationzim/geo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self,email,password=None,**other_user_fields):
+        user = self.model(email=email, **other_user_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+ 
+class User(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    
+    objects = UserManager()
--- api/populationzim/geo/tests.py
+++ api/populationzim/geo/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+
+        email = "test@ravira.com"
+        password = "raviraindezvenyu"
+
+        user = get_user_model().objects.create_user(email=email,password=password)
+
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password(password))
+

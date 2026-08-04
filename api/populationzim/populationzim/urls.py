98f2c163459fcf964d7bbb962e9a912683f6a872
tendaiwbm <tmbwanda52@gmail.com>
2026-08-04 13:56:02 +0200

    api: configuring auto-generation of API docs and the url to use to view the api documentation

--- api/populationzim/populationzim/urls.py
+++ api/populationzim/populationzim/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/geo/', include('geo.urls')),
-    
+    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="api-schema"), name="api-docs")
 ]
 
 if settings.DEBUG:

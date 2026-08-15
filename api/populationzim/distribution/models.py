690317582f1f701e93f2c55a1d5a260fdb6c94b9
tendaiwbm <tmbwanda52@gmail.com>
2026-08-15 12:55:27 +0200

    api: **distribution - configuring ward model kuti django isagadzire mufananidzo re model iyi      **distribution - adding non-spatial fields for ward model      **distribution - adding geometry field for ward model      **distribution - specifying/overriding schema and table names for ward model

--- api/populationzim/distribution/models.py
+++ api/populationzim/distribution/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.conf import settings
 
-# Create your models here.
+
+class Ward(models.Model):
+    ward_number = models.IntegerField(primary_key=True)
+    district_name = models.CharField(max_length=30)
+    province_name = models.CharField(max_length=30)
+
+    male_population_2022 = models.IntegerField()
+    female_population_2022 = models.IntegerField()
+    total_population_2022 = models.IntegerField()
+    male_population_2012 = models.IntegerField()
+    female_population_2012 = models.IntegerField()
+    total_population_2012 = models.IntegerField()
+
+    total_households_2022 = models.IntegerField()
+    total_households_2012 = models.IntegerField()
+    avg_householdsize_2022 = models.IntegerField()
+    avg_householdsize_2012 = models.IntegerField()
+
+    geom = ArrayField(ArrayField(ArrayField(ArrayField(models.DecimalField()))))
+
+    class Meta:
+        managed = False
+        db_table = f'"{settings.BASE_SCHEMA}"."ward"'
+        unique_together = [["ward_number","district_name"]]
+
+    objects = models.Manager()

4216b11d53bbe1690887a35152aeb57d0fdf53e5
tendaiwbm <tmbwanda52@gmail.com>
2026-08-17 10:31:40 +0200

    api: distribution - tweaks to finalize validation of ward population distribution query string

--- api/populationzim/distribution/views.py
+++ api/populationzim/distribution/views.py
@@ -1,5 +1,13 @@
 from django.http import JsonResponse
+from .validators import WardRequestValidator
 
 
 def get_ward_population(request):
-    return JsonResponse({"key": "value"})
+    
+    if request.method == "GET":
+        ward_request = WardRequestValidator(request.GET)
+        
+        if ward_request.is_valid():
+            return JsonResponse({"key": "valid"})
+
+    return JsonResponse({"key": "invalid"})

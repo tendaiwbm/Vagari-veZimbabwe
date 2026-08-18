dfa4fbed6f01ca228c500620f7434cfaeb97207e
tendaiwbm <tmbwanda52@gmail.com>
2026-08-18 20:19:42 +0200

    api: distribution - completing request > response cycle for district population distribution

--- api/populationzim/distribution/views.py
+++ api/populationzim/distribution/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
+from django.db.models import Sum
+from pandas import DataFrame
 from .validators import WardRequestValidator,DistrictRequestValidator
-from .models import Ward
+from .models import Ward,District
 
 
 def get_ward_population(request):
@@ -31,6 +33,7 @@ def get_ward_population(request):
 
     return JsonResponse({"message": "invalid request"})
 
+
 def get_district_population(request):
     """Handle requests for district population data"""
 
@@ -41,9 +44,23 @@ def get_district_population(request):
             params = district_request.cleaned_data
             population_field = "_".join([params["sex"],"population",str(params["year"])])            
             
-
+            wards = Ward.objects
+            districts = District.objects
+            if params["apply_filter"] and params["filter_district"]:
+                wards = wards.filter(district_name__in=params["filter_district"])
+                districts = districts.filter(district_name__in=params["filter_district"])
             
-            return JsonResponse({"message": "valid"})
+            wards = wards.values("district_name").annotate(district_population=Sum(population_field))
+            wards = DataFrame.from_records(wards)
+            districts = DataFrame.from_records(districts.values("district_name","geom"))
+            districts = districts.merge(wards,how="inner",on="district_name")
+            
+            response_dict = { "coordinates": [district if len(district[0]) == 1 
+                                              else [[district[0][0],[point for point in district[0][1] if point != [0,0]]]]
+                                              for district in districts["geom"]],
+                              "values": districts["district_population"].to_list(),
+                              "names": districts["district_name"].to_list() }
+            
+            return JsonResponse(response_dict)
 
     return JsonResponse({"message": "invalid request"})
-

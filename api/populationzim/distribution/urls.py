38798e2c5d3e26a9eeb57e76164dfbd8f521f520
tendaiwbm <tmbwanda52@gmail.com>
2026-08-21 12:29:45 +0200

    api: distribution - adding route to admin names endpoint

--- api/populationzim/distribution/urls.py
+++ api/populationzim/distribution/urls.py
@@ -4,4 +4,5 @@ from distribution import views
 
 urlpatterns = [path('ward/', views.get_ward_population),
                path('district/', views.get_district_population),
-               path('province/', views.get_province_population)]
+               path('province/', views.get_province_population),
+               path('names/', views.get_admin_names)]

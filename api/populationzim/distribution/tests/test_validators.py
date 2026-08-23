b34cff7f64104f1e2bb90a751da6207d1da91231
tendaiwbm <tmbwanda52@gmail.com>
2026-08-23 10:02:51 +0200

    api: distribution - test for missing url parameter year

--- api/populationzim/distribution/tests/test_validators.py
+++ api/populationzim/distribution/tests/test_validators.py
@@ -37,3 +37,16 @@ class FormValidationTests(TestCase):
         
         validator = v.BaseDistributionRequestValidator(params)
         self.assertTrue(validator.is_valid())
+    
+    def test_year_missing(self):
+        """Test validation raises error when year not in query string"""
+
+        params = deepcopy(self.query_string_parameters)
+        params_to_drop = ("filter_district","filter_province","apply_filter","year")
+        for param in params_to_drop:
+            params.pop(param)
+        
+        validator = v.BaseDistributionRequestValidator(params)
+        with self.assertRaises(AssertionError):
+            self.assertTrue(validator.is_valid())
+ 

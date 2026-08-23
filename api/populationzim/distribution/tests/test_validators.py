d26d466bbb8e4c61c2e0ec3e781cd0d4d566a82e
tendaiwbm <tmbwanda52@gmail.com>
2026-08-23 09:55:19 +0200

    api: distribution - test for missing url parameter admin_level

--- api/populationzim/distribution/tests/test_validators.py
+++ api/populationzim/distribution/tests/test_validators.py
@@ -1,7 +1,29 @@
+from copy import deepcopy
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+import distribution.validators as v
 
 
 class FormValidationTests(TestCase):
-
+    """Tests for validation forms"""
+    
     def setUp(self):
-        return
+        self.query_string_parameters = { "admin_level": "ward",
+                                         "sex": "total",
+                                         "year": 2022,
+                                         "apply_filter": "true",
+                                         "filter_district": "Mazowe,Mvurwi",
+                                         "filter_province": "Matebeleland North,Matebeleland South" }
+
+    def test_admin_level_missing(self):
+        """Test validation raises error when admin_level not in query string"""
+
+        params = deepcopy(self.query_string_parameters)
+        params_to_drop = ("filter_district","filter_province","apply_filter","admin_level")
+        for param in params_to_drop:
+            params.pop(param)
+        
+        validator = v.BaseDistributionRequestValidator(params)
+        with self.assertRaises(AssertionError):
+            self.assertTrue(validator.is_valid())
+        

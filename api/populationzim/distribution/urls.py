5792bf52a0713425bc3f5991b3ffd5e95c257ff8
tendaiwbm <tmbwanda52@gmail.com>
2026-08-25 08:27:28 +0200

    api: distribution - correct alias for get_province_population

--- api/populationzim/distribution/urls.py
+++ api/populationzim/distribution/urls.py
@@ -6,5 +6,5 @@ app_name = "distribution"
 
 urlpatterns = [path('ward/', views.get_ward_population, name="ward_population"),
                path('district/', views.get_district_population, name="district_population"),
-               path('province/', views.get_province_population, name="province_distribution"),
+               path('province/', views.get_province_population, name="province_population"),
                path('names/', views.get_admin_names, name="admin_names")]

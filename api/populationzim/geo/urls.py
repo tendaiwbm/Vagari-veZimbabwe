1299d31daf85fe7ec462d4a225213abbe5455dd9
tendaiwbm <tmbwanda52@gmail.com>
2026-08-26 12:37:00 +0200

    api: removing old geo app now replaced by distribution

--- api/populationzim/geo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import Distribution,fetch_admin_names
-
-urlpatterns = [
-        path('',Distribution.as_view()),
-        path('zvakavanda/',fetch_admin_names),
-
-]
-

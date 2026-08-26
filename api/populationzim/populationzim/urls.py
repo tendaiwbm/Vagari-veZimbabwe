60089420fe82fcf171ec188f60fc509d4078259d
tendaiwbm <tmbwanda52@gmail.com>
2026-08-26 12:37:00 +0200

    api: **removing old geo app now replaced by distribution      **populationzim - removing url forward to deprecated geo app

--- api/populationzim/populationzim/urls.py
+++ api/populationzim/populationzim/urls.py
@@ -5,5 +5,4 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [path('admin/', admin.site.urls),
                path("api/rondedzero/", SpectacularSwaggerView.as_view(url_name="api-schema"), name="api-docs"),
                path("api/muviri/", SpectacularAPIView.as_view(), name="api-schema"),
-               path('api/geo/', include('geo.urls')),
                path("api/distribution/",include("distribution.urls"))]
